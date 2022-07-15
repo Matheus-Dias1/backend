0336820b2c88e1f4767fd9f9d831adc7466dcb8a
Matheus Dias <reptarop@gmail.com>
2022-07-15 16:51:37 -0300

    changed port for heroku

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Listening on port ${port}`);
 });
