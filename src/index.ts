f1826a990fd1e53e097a99d2b7999b0cd597a56a
Matheus Dias <reptarop@gmail.com>
2022-07-15 16:39:18 -0300

    added procfile

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import router from "./routes";
 import cors from "cors";
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.tjb515h.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
 mongoose.connect(uri);
